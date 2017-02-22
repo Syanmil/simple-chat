fb4de2ee2070dd087fa9715fdfdc2ceb681706db
Syanmil <syanmil@yahoo.com>
2017-02-23 00:20:54 +0700

    simple chat done

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-var http = require('http'),
-    fs = require('fs'),
-    // NEVER use a Sync function except at start-up!
-    index = fs.readFileSync(__dirname + '/index.html');
+var http = require('http')
+var fs = require('fs')
+var index = fs.readFileSync(__dirname + '/index.html');
+var express = require('express')
+var route = express()
 
 // Send index.html to all requests
 var app = http.createServer(function(req, res) {
@@ -9,23 +10,21 @@ var app = http.createServer(function(req, res) {
     res.end(index);
 });
 
+route.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
+
 // Socket.io server listens to our app
 var io = require('socket.io').listen(app);
 
-// Send current time to all connected clients
-function sendTime() {
-    io.emit('time', { time: new Date().toJSON() });
-}
-
-// Send current time every 10 secs
-setInterval(sendTime, 10000);
-
 // Emit welcome message on connection
 io.on('connection', function(socket) {
-    // Use socket to communicate with this particular client only, sending it it's own id
-    socket.emit('welcome', { message: 'Welcome!', id: socket.id });
-
-    socket.on('i am client', console.log);
+    socket.on('chating', function(chat){
+        io.emit('chating', chat)
+    });
+    socket.on('disconnect', function(chat){
+      console.log('disconnected');
+    });
 });
 
 app.listen(3000);
